163af524082c1745db302d674f49345b431ec06c
OsmoxX <125191904+OsmoxX@users.noreply.github.com>
2023-02-13 22:03:48 +0100

    to add views:
    -about_efekt
    -certificates
    -contact
    -gallery
    -offers
    -references

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
-  def index 
-  end
+  def index; end
+  def about_efekt; end
+  def offers; end
+  def gallery; end
+  def references; end
+  def certificates; end
+  def contact; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
+  get :about_efekt, to: 'home#about_efekt'
+  get :offers, to: 'home#offers'
+  get :galery, to: 'home#galery'
+  get :references, to: 'home#references'
+  get :certificates, to: 'home#certificates'
+  get :contact, to: 'home#contact'
 end
